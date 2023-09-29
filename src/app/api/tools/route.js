f87272f4018e4846133a657a5e8357a90415cdb4
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2023-09-30 01:37:18 +0600

    [Create]: api folder init done

---
+++ src/app/api/tools/route.js
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+
+// http://localhost:3001/api/tools
+// GET
+export async function GET(req, res) {
+  return NextResponse.json({ message: "Hello world I'm GET Method" });
+}
+
+// POST
+export async function POST(req, res) {
+  return NextResponse.json({ message: "Hello world I'm POST Method" });
+}
+
+// PUT
+export async function PUT(req, res) {
+  return NextResponse.json({ message: "Hello world I'm PUT Method" });
+}
+
+// DELETE
+export async function DELETE(req, res) {
+  return NextResponse.json({ message: "Hello world I'm DELETE Method" });
+}
