fd2765dd9bd05a81d3104b4e4aa66f003581c759
mahedi <mahedianik4@gmail.com>
2023-10-04 19:43:23 +0600

    add login diret nabar , design favarote page design , dasboard root page design

---
+++ src/app/favorites/page.js
@@ -0,0 +1,83 @@
+"use client";
+import React, { useState } from "react";
+const FavoritesPage = () => {
+  const [activeTab, setActiveTab] = useState("tab1");
+  const handleTabClick = (tabId) => {
+    setActiveTab(tabId);
+  };
+ 
+  return (
+    <div className=" px-2 mt-6">
+      
+      <div >
+      <h1 className="text-3xl font-semibold" >Your AI Favorites.</h1>
+      <p className="pt-2">
+        These are the tools and posts you have favourited. You can remove them
+        from your favourites by clicking the bookmark icon.
+      </p>
+      </div>
+
+      <div className="mt-24">
+        <div className="tabs flex justify-center">
+          <a
+            id="tab1"
+            className={`${
+              activeTab === "tab1"
+                ? "tab tab-lg tab-bordered tab-active"
+                : "tab tab-lg tab-bordered"
+            }`}
+            onClick={() => handleTabClick("tab1")}
+          >
+           AI TOOLS
+          </a>
+          <a
+            id="tab2"
+            className={` ${
+              activeTab === "tab2"
+                ? "tab tab-lg tab-bordered tab-active"
+                : "tab tab-lg tab-bordered"
+            }`}
+            onClick={() => handleTabClick("tab2")}
+          >
+           AI NEWS
+          </a>
+          <a
+            id="tab3"
+            className={` ${
+              activeTab === "tab3"
+                ? "tab tab-lg tab-bordered tab-active"
+                : "tab tab-lg tab-bordered"
+            }`}
+            onClick={() => handleTabClick("tab3")}
+          >
+           AI PLUGINS
+          </a>
+        </div>
+        <div className="tabs_content">
+          <div
+            id="tab1_content"
+            style={{ display: activeTab === "tab1" ? "block" : "none" }}
+          >
+            <div>
+            No ai Tools Favorited Yet.
+            </div>
+          </div>
+          <div
+            id="tab2_content"
+            style={{ display: activeTab === "tab2" ? "block" : "none" }}
+          >
+          No News Favourited Yet
+          </div>
+          <div
+            id="tab3_content"
+            style={{ display: activeTab === "tab3" ? "block" : "none" }}
+          >
+        No Plugins Favourited Yet
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FavoritesPage;
--- src/components/Shared/Navbar.js
+++ src/components/Shared/Navbar.js
@@ -1,15 +1,15 @@
 "use client";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { useTheme } from "next-themes";
+import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
 import { ThemeChanger } from "../Providers/ThemeChanger";
-import { signOut, useSession } from "next-auth/react";
 const Navbar = () => {
-  const [open, setOpen] = useState(false);
-
-  const { status } = useSession();
+  const { theme } = useTheme();
+  const { status, data } = useSession();
   return (
-    <div className="border-b h-18 bg-base-100">
-      <div className="navbar max-w-[1280px] mx-auto">
+    <div className="border-b h-18 bg-base-100 sticky top-0 left-0 right-0 z-10">
+      <div className="navbar max-w-[1280px] mx-auto ">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -81,25 +81,42 @@ const Navbar = () => {
               </li>
             </ul>
           </div>
-          <Link href="/" className="normal-case text-xl">
-            YOUR LOGO
-          </Link>
+          {theme === "dark" ? (
+            <Link href="/" className="w-full">
+              <Image
+                src="/logo-dark.png"
+                loading="lazy"
+                width={110}
+                height={110}
+                alt="jack logo"
+                sizes="(max-width: 768px) 100vw"
+              />
+            </Link>
+          ) : (
+            <Link href="/" className="w-full">
+              <Image
+                src="/logo-dafult.png"
+                loading="lazy"
+                width={110}
+                height={110}
+                alt="jack logo"
+                sizes="(max-width: 768px) 100vw"
+              />
+            </Link>
+          )}
         </div>
 
         {/* large device */}
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1 text-md uppercase font-semibold">
             <li>
-              <a>Favorites</a>
-            </li>
-            <li>
-              <a>Discover</a>
+              <Link href="/favorites">Favorites</Link>
             </li>
             <li className="dropdown dropdown-hover">
               <label tabIndex={0}>Submit</label>
               <ul
                 tabIndex={0}
-                className="dropdown-content z-[1] menu p-2 shadow bg-base-100 text-md font-light w-36"
+                className="dropdown-content z-[1] menu p-2 shadow bg-base-100 text-md w-36"
               >
                 <li>
                   <a>Submit Tool</a>
@@ -110,7 +127,7 @@ const Navbar = () => {
               <label tabIndex={0}>Resources</label>
               <ul
                 tabIndex={0}
-                className="dropdown-content z-[1] menu p-2 shadow bg-base-100 w-52 text-md font-light"
+                className="dropdown-content z-[1] menu p-2 shadow bg-base-100 w-52 text-md "
               >
                 <li>
                   <a>Newsletter Issues</a>
@@ -132,24 +149,55 @@ const Navbar = () => {
             <li>
               <a>Subscribe</a>
             </li>
+            <li>
+              <a>Contact</a>
+            </li>
           </ul>
         </div>
         <div className="navbar-end">
           <ThemeChanger />
-          {status === "unauthenticated" ? (
-            <Link className="btn btn-xs ml-3" href="/login">
+          
+{status === "loading" ? <span className="loading loading-ring loading-lg"></span>:<>
+{status === "unauthenticated" ? (
+            <div
+              className="flex justify-center items-center cursor-pointer"
+              onClick={() => signIn("google")}
+            >
+              <div className="w-8 ">
+                <img src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png" />
+              </div>
               Login
-            </Link>
+            </div>
           ) : (
             <>
-              <span
-                className="btn btn-outline btn-primary ml-3"
-                onClick={signOut}
-              >
-                Logout
-              </span>
+              <div className="dropdown dropdown-end">
+                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                  <div className="w-10 rounded-full">
+                    <img src={data?.user?.image} />
+                  </div>
+                </label>
+                <ul
+                  tabIndex={0}
+                  className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+                >
+                  <li>
+                    <Link href="/dashboard" className="justify-between ">
+                      Dashboard
+                    </Link>
+                  </li>
+                  <li>
+                    <a className="">Settings</a>
+                  </li>
+                  <li>
+                    <button className="" onClick={signOut}>
+                      Logout
+                    </button>
+                  </li>
+                </ul>
+              </div>
             </>
-          )}
+          )}</>}
+          
         </div>
       </div>
     </div>
