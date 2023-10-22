08fefa28f5dfc9864991531f494081bb9601fa51
mahedi <mahedianik4@gmail.com>
2023-10-23 01:51:36 +0600

    solve news filtering api

--- src/app/api/news/route.js
+++ src/app/api/news/route.js
@@ -1,5 +1,6 @@
 import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
+import { pick } from "../helpers";
 
 // post 
 export async function POST(req, res) {
@@ -13,8 +14,93 @@ export async function POST(req, res) {
   }
 // GET ALL NEWS 
 export async function GET(req, res) {
+    const newsFilterableFields = ["category", "sort", "time"];
+    const { searchParams } = new URL(req.url);
+    const query = Array.from(searchParams).reduce((result, [key, value]) => {
+      if (result[key]) {
+        if (Array.isArray(result[key])) {
+          result[key].push(value);
+        } else {
+          result[key] = [result[key], value];
+        }
+      } else {
+        result[key] = value;
+      }
+      return result;
+    }, {});
+    
+    const filters = pick(query, newsFilterableFields);
+    const { category, sort, time } = filters;
+    console.log(category)
+    const andConditions = [];
+  
+    if (category) {
+      andConditions.push({
+        newsCategory: {
+          equals: category,
+        },
+      });
+    }
+  
+    if (sort) {
+      // Implement sorting based on the 'sort' parameter
+      if (sort === "new") {
+        // Sort by the 'createdAt' field to get the newest articles first
+        andConditions.push({
+          createdAt: {
+            gte: new Date(Date.now() - 24 * 60 * 60 * 1000),
+          },
+        });
+      } else if (sort === "popular") {
+        // Customize sorting for "Popular" here, e.g., sorting by views
+        andConditions.push({
+          views: {
+            gte: 0,
+          },
+        });
+      }
+    }
+    
+    if (time) {
+      // Adjust time filtering based on the 'time' parameter
+      if (time === "today") {
+        // Filter for articles published within the last 24 hours
+        andConditions.push({
+          createdAt: {
+            gte: new Date(Date.now() - 24 * 60 * 60 * 1000),
+          },
+        });
+      } else if (time === "week") {
+        // Filter for articles published within the current week
+        const startOfWeek = new Date();
+        startOfWeek.setDate(startOfWeek.getDate() - startOfWeek.getDay());
+        startOfWeek.setHours(0, 0, 0, 0);
+        andConditions.push({
+          createdAt: {
+            gte: startOfWeek,
+          },
+        });
+      } else if (time === "month") {
+        // Filter for articles published within the current month
+        const startOfMonth = new Date();
+        startOfMonth.setDate(1);
+        startOfMonth.setHours(0, 0, 0, 0);
+        andConditions.push({
+          createdAt: {
+            gte: startOfMonth,
+          },
+        });
+      }
+    }
+      const whereConditions = andConditions.length > 0 ? { AND: andConditions } : {};
+      console.log(whereConditions)
     try {
-        const result = await prisma.News.findMany({})
+
+        const result = await prisma.News.findMany({
+            where: whereConditions,
+           
+          });
+          
         return NextResponse.json({ message: "successfully get news",result },{status:200},);
     } catch (error) {
         return NextResponse.json({ message: "filed",error:error.message },{status:500});
--- src/app/news/page.js
+++ src/app/news/page.js
@@ -1,130 +1,197 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BiSolidDownArrow } from "react-icons/bi";
 import { BsBookmarkPlus, BsTools } from "react-icons/bs";
 import { LuExternalLink } from "react-icons/lu";
+const newsCategory = [
+  { category: "ai" },
+  { category: "ai" },
+  { category: "ai" },
+  { category: "video" },
+  { category: "ai" },
+  { category: "ai" },
+];
 const NewsCard = () => {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-
-
-  
+  const [isTimeDropdownOpen, setIsTimeDropdownOpen] = useState(false);
+  const [isCategoryDropdownOpen, setIsCategoryDropdownOpen] = useState(false);
+  const [isSortDropdownOpen, setIsSortDropdownOpen] = useState(false);
+  const [selectedSort, setSelectedSort] = useState("");
+  const [selectedTime, setSelectedTime] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [newsData, setNewsData] = useState([]);
   const link =
     "https://stackoverflow.com/questions/72443582/how-to-identify-react-components-with-the-useinview-hook-and-pass-their-values-t";
+
+  const handleTimeDropdownToggle = () => {
+    setIsTimeDropdownOpen(!isTimeDropdownOpen);
+    // Close other dropdowns if open
+    setIsCategoryDropdownOpen(false);
+    setIsSortDropdownOpen(false);
+  };
+
+  const handleCategoryDropdownToggle = () => {
+    setIsCategoryDropdownOpen(!isCategoryDropdownOpen);
+    // Close other dropdowns if open
+    setIsTimeDropdownOpen(false);
+    setIsSortDropdownOpen(false);
+  };
+
+  const handleSortDropdownToggle = () => {
+    setIsSortDropdownOpen(!isSortDropdownOpen);
+    // Close other dropdowns if open
+    setIsTimeDropdownOpen(false);
+    setIsCategoryDropdownOpen(false);
+  };
+  const applyFilters = () => {
+    const queryParams = [];
+
+    if (selectedSort) {
+      queryParams.push(`sort=${selectedSort}`);
+    }
+
+    if (selectedTime) {
+      queryParams.push(`time=${selectedTime}`);
+    }
+
+    if (selectedCategory) {
+      queryParams.push(`category=${selectedCategory}`);
+    }
+    const apiUrl = `/api/tools${
+      queryParams.length > 0 ? `?${queryParams.join("&")}` : ""
+    }`;
+    console.log(apiUrl);
+  };
+  const handleSortOptionClick = (option) => {
+    setSelectedSort(option);
+    setIsSortDropdownOpen(false);
+    applyFilters();
+  };
+
+  const handleTimeFilterClick = (option) => {
+    setSelectedTime(option);
+    setIsTimeDropdownOpen(false);
+    applyFilters();
+  };
+
+  const handleCategoryFilterClick = (option) => {
+    setSelectedCategory(option);
+    setIsCategoryDropdownOpen(false);
+    applyFilters();
+  };
+
+  // Handle filtering options selection for time, category, and sorting
+
   return (
-    <div>
-      <h3 className="text-4xl my-6">Latest AI News | All Time</h3>
-      <div className="flex justify-between my-12">
-        <div className="flex space-x-11">
-          <div className="relative ">
-            <button
-              className="btn  rounded-full btn-outline "
-             
-            >
-              Filer by Time: <BiSolidDownArrow />
-            </button>
-            {isDropdownOpen && (
-              <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-36 absolute">
-                <li>
-                  <button
-                    className="w-full"
-                    
-                  >
-                    Today
-                  </button>
-                </li>
-                <li>
-                  <button
-                    className="w-full"
-                   
-                  >
-                   This Week 
-                  </button>
-                </li>
-                <li>
-                  <button
-                    className="w-full"
-                
-                  >
-                    This month 
-                  </button>
-                </li>
-                <li>
-                  <button
-                    className="w-full"
-                   
-                  >
-                    All time
-                  </button>
-                </li>
-              </ul>
-            )}
-          </div>
-          <div className="relative ">
-            <button
-              className="btn  rounded-full btn-outline"
-              
-            >
-            Filer by Category:  <BiSolidDownArrow />
-            </button>
-            {isDropdownOpen && (
-              <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-36 absolute">
-                <li>
-                  <button
-                    className="w-full"
-                    onClick={() => handleSortOptionClick("Verified")}
-                  >
-                    Verified
-                  </button>
-                </li>
-                <li>
-                  <button
-                    className="w-full"
-                    onClick={() => handleSortOptionClick("New")}
-                  >
-                    New
-                  </button>
-                </li>
-                <li>
-                  <button
-                    className="w-full"
-                    onClick={() => handleSortOptionClick("Popular")}
-                  >
-                    Popular
-                  </button>
-                </li>
-              </ul>
-            )}
-          </div>
-        </div>
-        <div className="relative ">
+    <div className="p-4 lg:p-8 xl:p-12">
+    
+      {/* Apply padding for different devices */}
+      <h3 className="text-2xl md:text-3xl lg:text-4xl my-4 md:my-6">
+        Latest AI News | All Time
+      </h3>
+      <div className="flex flex-col md:flex-row justify-between  my-4 md:my-8">
+        <div className="lg:flex lg:space-x-8  justify-center items-center">
+        <div className="relative mb-4 md:mb-0">
+          {" "}
+          {/* Margin for spacing */}
           <button
-            className="btn lg:px-12 rounded-full btn-outline "
-            
+            className="btn rounded-full btn-outline"
+            onClick={handleTimeDropdownToggle}
           >
-            Sort by:  <BiSolidDownArrow />
+            Filter by Time <BiSolidDownArrow />
           </button>
-          {isDropdownOpen && (
-            <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-36 absolute">
+          {isTimeDropdownOpen && (
+            <ul className="p-2 shadow menu dropdown-content z-10 bg-base-100 rounded-box w-36 absolute md:w-48 xl:w-60">
+              {" "}
+              {/* Adjust dropdown width */}
               <li>
                 <button
+                  onClick={() => handleTimeFilterClick("today")}
                   className="w-full"
+                >
+                  Today
+                </button>
+              </li>
+              <li>
+                <button
+                  onClick={() => handleTimeFilterClick("week")}
+                  className="w-full"
+                >
+                  This Week
+                </button>
+              </li>
+              <li>
+                <button
+                  onClick={() => handleTimeFilterClick("month")}
+                  className="w-full"
+                >
+                  This Month
+                </button>
+              </li>
+              <li>
+                <button
+                  onClick={() => handleTimeFilterClick("allTime")}
+                  className="w-full"
+                >
+                  All Time
+                </button>
+              </li>
+            </ul>
+          )}
+        </div>
+
+        <div className="relative mb-4 md:mb-0">
+          <button
+            className="btn rounded-full btn-outline"
+            onClick={handleCategoryDropdownToggle}
+          >
+            Filter by Category <BiSolidDownArrow />
+          </button>
+          {isCategoryDropdownOpen && (
+            <ul className="p-2 shadow menu dropdown-content z-10 bg-base-100 rounded-box w-36 absolute md:w-48 xl:w-60">
+              {newsCategory?.map((cate, index) => (
+                <li key={index}>
+                  <button
+                    onClick={() => handleCategoryFilterClick(cate.category)}
+                    className="w-full"
+                  >
+                    {cate.category}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        </div>
+
+        <div className="relative">
+          <button
+            className="btn lg:px-6 xl:px-12 rounded-full btn-outline"
+            onClick={handleSortDropdownToggle}
+          >
+            Sort by <BiSolidDownArrow />
+          </button>
+          {isSortDropdownOpen && (
+            <ul className="p-2 shadow menu dropdown-content z-10 bg-base-100 rounded-box w-36 absolute md:w-48 xl:w-60">
+              <li>
+                <button
                   onClick={() => handleSortOptionClick("Verified")}
+                  className="w-full"
                 >
                   Verified
                 </button>
               </li>
               <li>
                 <button
+                  onClick={() => handleSortOptionClick("new")}
                   className="w-full"
-                  onClick={() => handleSortOptionClick("New")}
                 >
                   New
                 </button>
               </li>
               <li>
                 <button
+                  onClick={() => handleSortOptionClick("popular")}
                   className="w-full"
-                  onClick={() => handleSortOptionClick("Popular")}
                 >
                   Popular
                 </button>
@@ -133,23 +200,23 @@ const NewsCard = () => {
           )}
         </div>
       </div>
-      
-      <div className="card border mt-6 ">
+      {/* Rest of your card content */}
+      <div className="card border mt-4 md:mt-8 xl:mt-12">
         <div className="card-body">
           <a
             target="_blank"
             href={link}
-            className=" box-content overflow-hidden"
+            className="text-sm md:text-base lg:text-lg xl:text-xl box-content overflow-hidden flex"
           >
             <span className="hover:text-secondary">
-              text-secondary hover:text-secondary hover:text-secondary
+             title asdkfjhas frtasdi
             </span>{" "}
             <span className="text-secondary flex-wrap">
               {link.slice(0, 50)}
             </span>
             .... <LuExternalLink className="inline"></LuExternalLink>{" "}
           </a>
-          <div className="flex flex-col mt-6 md:flex-row md:items-center md:justify-between">
+          <div className="flex flex-col md:flex-row md:items-center md:justify-between mt-4 md:mt-6 xl:mt-8">
             <div className="text-secondary md:mt-0">12 hours ago</div>
             <button className="bg-gray-200 rounded-lg px-6 mt-4 md:mt-0 text-black font-light">
               Interesting
